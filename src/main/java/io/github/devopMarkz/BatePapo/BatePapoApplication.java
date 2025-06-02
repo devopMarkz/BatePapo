38cdd2531d1e423a7a8a53b5868f86bd2d152ccb
devopMarkz <marcosdev2002@gmail.com>
2025-06-02 20:19:54 -0300

    first commit

---
+++ src/main/java/io/github/devopMarkz/BatePapo/BatePapoApplication.java
@@ -0,0 +1,13 @@
+package io.github.devopMarkz.BatePapo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatePapoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatePapoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/devopMarkz/BatePapo/BatePapoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devopMarkz.BatePapo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatePapoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
